80a51184813e046b8e0cabdbbbb30e931af4efdc
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2022-12-01 16:11:54 +0330

    fix: remove redunant packages and clean up

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -4,7 +4,6 @@ import type { ReactNode } from 'react';
 import Footer from '@/components/Main/footer/Footer';
 import Header from '@/components/Main/header/Header';
 import type { Tab } from '@/components/Main/header/Header.types';
-import SearchBox from '@/components/Main/searchbox/SearchBox';
 import styles from '@/styles/main/main.module.scss';
 
 type IMainProps = {
@@ -20,7 +19,6 @@ const Main = (props: IMainProps) => (
     {props.meta}
     <Header tab={props.selectedTab} />
     <div className="mx-auto max-w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
-      <SearchBox title={props.title} />
       <div
         className={cn({
           [styles.content as any]: true,
